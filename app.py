e3851170ecda822e0e277fe29f8e414760e1ee6a
rguilcas <r.guilcas@outlook.com>
2026-02-06 11:21:47 +0100

    update app.py file

--- app.py
+++ app.py
@@ -11,21 +11,16 @@ from holoviews import streams
 
 pn.extension(design="material", sizing_mode="stretch_width")
 
+
 @pn.cache
 def get_data():
     ds1= xr.open_dataset("data/LENS_precomputed_terms_combined.nc")
     gdf_regions = gpd.read_file('data/rainfall_regions.geojson',).set_index('index')
     ds1 = ds1.assign_coords(region_id = gdf_regions.index.values)
     ds1 = xr.concat([ds1,ds1.sum('source').assign_coords(source='all')], dim='source') 
-    
-    # gdf_regions.index = [K39_regions[k] for k in gdf_regions.index]
-    
-
     return ds1, gdf_regions
 
 ds1, gdf_regions = get_data()
-# data.tail()
-# data.tail()
 
 
 def get_plot(model='CESM2', season='DJF', term='bias', source='dyn', vmax=50):
@@ -38,12 +33,11 @@ def get_plot(model='CESM2', season='DJF', term='bias', source='dyn', vmax=50):
 
     gdf = gdf_regions.copy()
     gdf[term] = ds_sel.individual_term.to_series()
-    
     # hover = HoverTool(tooltips=tooltips)
     # hover = HoverTool(tooltips = [('@index', '@term')])
     poly_plot = gdf.hvplot.polygons( crs=ccrs.PlateCarree(), color=term)\
-        .opts(height=600, 
-              width=600, 
+        .opts(height=500, 
+              width=500, 
               # tools=['hover'],
               tools=['tap', 'hover'],  # Ensure tap is here
               # selection_mode='single',   # Or 'multiple'
@@ -61,19 +55,19 @@ def get_plot(model='CESM2', season='DJF', term='bias', source='dyn', vmax=50):
         if not index:
             return "### Please select a region"
         # print(index)
-        regions = list(gdf.iloc[index].index)
-        region_names='  \n'+'  \n'.join(regions)
+        regions = gdf.iloc[index].index
+        # region_names='  \n'+'  \n'.join(regions)
         # data=gdf.loc[index].index
         ds_sel_barplot = (
                 ds1
                 .sel(model=model, season=season, term=term,  region_id=regions)
                 .sum("synoptic_bin") / 0.05 * 100
             ).individual_term
-        return ds_sel_barplot
-        series =  ds_sel_barplot.to_series()#
-        series.index = series.index.reorder_levels([1,0])
-        bars = series.hvplot.bar(title=term)
-        return bars.opts(height=800, 
+        series =  ds_sel_barplot.to_series().unstack().T#
+        # series.index = series.index.reorder_levels([1,0])
+
+        bars = series.hvplot.barh(title=term)
+        return bars.opts(height=500, 
                          width=200)*hv.HLine(0).opts(color='k')#f"### You clicked on:  \n{region_names}"
 
     
@@ -101,6 +95,7 @@ source_widget = pn.widgets.RadioButtonGroup(
     options=list(ds1.source.values),
 )
 vmax_widget = pn.widgets.IntSlider(name="Colormap max value", value=50, start=1, end=150)
+
 
 bound_plot = pn.bind(
     get_plot, model=model_widget, season=season_widget, term=term_widget, source=source_widget, vmax=vmax_widget
